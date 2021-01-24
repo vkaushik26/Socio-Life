5277086141935d2cd8d198cd10789a1cab996cd6
vaibhav kaushik <kaushikvaibhav26@gmail.com>
2021-01-24 17:56:42 +0530

    First commit

---
+++ public/precache-manifest.58f8f9befef928425de6f2e9c3ad495d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "bfc616096344c2bf164a",
+    "url": "/static/js/main.bfc61609.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "863aa797365d9429ffd7",
+    "url": "/static/js/2.863aa797.chunk.js"
+  },
+  {
+    "revision": "b23d5960ea1aa29a34d567b62f9d1617",
+    "url": "/static/media/avatar.b23d5960.jpg"
+  },
+  {
+    "revision": "586806cd047e33b5ee7a43fc4252208d",
+    "url": "/static/media/mountains.586806cd.jpg"
+  },
+  {
+    "revision": "94742765d9692ecbee7bbc81afa4ae33",
+    "url": "/index.html"
+  }
+];
