ae618c4a71f109451956bcbb4f30a2101fdd5de0
vaibhav kaushik <kaushikvaibhav26@gmail.com>
2021-01-24 19:37:50 +0530

    Update post.js

--- routes/post.js
+++ routes/post.js
@@ -17,7 +17,7 @@ const {
 } = require('../controllers/post');
 const { requireSignin } = require('../controllers/auth');
 const { userById } = require('../controllers/user');
-const { createPostValidator } = require('../validator');
+const { createPostValidator } = require('../Validator');
 
 const router = express.Router();
 
